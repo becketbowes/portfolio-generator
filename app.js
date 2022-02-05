1837c17484815c0ddb8bbffe6ed02cb65f88189c
becket <becket.bowes@gmail.com>
2022-02-05 17:02:49 -0500

    first generated question

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+    }
+  ])
+  .then((answers) => console.log(answers))
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log('Prompt couldnt be rendered in the current environment');
+    } else {
+      console.log('Something else went wrong');
+    }
+  });
 
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const [userName, githubName] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('index.html', generatePage(userName, githubName), err => {
-  if (err) throw err;
-  
-  console.log("Portfolio complete! Checkout index.html to see the output!")
-});
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
---
+++ archive/app.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [userName, githubName] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(userName, githubName), err => {
+  if (err) throw err;
+  
+  console.log("Portfolio complete! Checkout index.html to see the output!")
+});
